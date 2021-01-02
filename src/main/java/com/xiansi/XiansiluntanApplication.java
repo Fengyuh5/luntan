a71404242d9c3bbcd22ee0754e0b9754a47cbdf5
xiansi <285466652@qq.com>
2021-01-02 20:53:29 +0800

    init repo

---
+++ src/main/java/com/xiansi/XiansiluntanApplication.java
@@ -0,0 +1,13 @@
+package com.xiansi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiansiluntanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XiansiluntanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xiansi/XiansiluntanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiansi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiansiluntanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
